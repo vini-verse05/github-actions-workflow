3f5f2b337fcd04da85e1f439ad88209e05290754
vini-verse05 <vini.prjpt05@gmail.com>
2026-08-17 16:35:20 +0530

    Initial Go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vini-verse05/github-actions-workflow
+
+go 1.26.6
---
+++ math_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+    result := Add(5, 3)
+
+    expected := 8
+
+    if result != expected {
+        t.Errorf("Expected %d but got %d", expected, result)
+    }
+}
